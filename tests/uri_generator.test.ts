81389d89076915301bd21d702f3bc27d7bd3d0e8
JJ Merelo <jjmerelo@gmail.com>
2023-10-02 14:53:14 +0200

    #3 creating initial infra

--- tests/uri_generator.test.ts
+++ tests/uri_generator.test.ts
@@ -4,10 +4,13 @@ import { assertExists } from "https://deno.land/std@0.174.0/testing/asserts.ts";
 import { URIgenerator } from "../lib/uri_generator.ts";
 
 // import user-agents.json
-const userAgentData = await Deno.readTextFile("./user-agents.json");
+const userAgentData = JSON.parse(
+  await Deno.readTextFile("tests/user-agents.json")
+);
+const aUriGenerator = new URIgenerator();
 
 Deno.test(function testURIgenerator() {
   for (const headerData of userAgentData) {
-    assertExists(URIgenerator(headerData));
+    assertExists(aUriGenerator.generateIdFromBrowser(headerData));
   }
 });

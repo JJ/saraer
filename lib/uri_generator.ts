600ac184f170754e2f4c34446944103a9c901be6
JJ Merelo <jjmerelo@gmail.com>
2023-10-02 17:38:59 +0200

    :white_check_mark: checking that user-agent digests differ #3

--- lib/uri_generator.ts
+++ lib/uri_generator.ts
@@ -1,6 +1,6 @@
 import { crypto } from "https://deno.land/std@0.203.0/crypto/mod.ts";
 
-interface HeaderData {
+export interface HeaderData {
   user_agent: string;
   accept: string;
   accept_language: string;
@@ -8,14 +8,19 @@ interface HeaderData {
 
 export class URIgenerator {
   private encoder: TextEncoder;
+  private decoder: TextDecoder;
 
   constructor() {
     this.encoder = new TextEncoder();
+    this.decoder = new TextDecoder();
   }
 
   generateIdFromBrowser({ user_agent, accept, accept_language }: HeaderData) {
     const collated = `${user_agent} | ${accept} | ${accept_language}`;
     const data = this.encoder.encode(collated);
-    return crypto.subtle.digestSync("SHA-256", data);
+    const hashArray = Array.from(
+      new Uint8Array(crypto.subtle.digestSync("SHA-256", data))
+    );
+    return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
   }
 }
--- tests/uri_generator.test.ts
+++ tests/uri_generator.test.ts
@@ -1,7 +1,7 @@
 // generate test for URIgenerator
-import { assertExists } from "https://deno.land/std@0.174.0/testing/asserts.ts";
+import { assertNotEquals } from "https://deno.land/std@0.174.0/testing/asserts.ts";
 
-import { URIgenerator } from "../lib/uri_generator.ts";
+import { HeaderData, URIgenerator } from "../lib/uri_generator.ts";
 
 // import user-agents.json
 const userAgentData = JSON.parse(
@@ -10,7 +10,21 @@ const userAgentData = JSON.parse(
 const aUriGenerator = new URIgenerator();
 
 Deno.test(function testURIgenerator() {
-  for (const headerData of userAgentData) {
-    assertExists(aUriGenerator.generateIdFromBrowser(headerData));
+  const pairsOfData: Array<Array<HeaderData>> = [];
+  for (const i in userAgentData) {
+    const headerData = userAgentData[i];
+    for (const j in userAgentData) {
+      if (j <= i) {
+        continue;
+      }
+      pairsOfData.push([headerData, userAgentData[j]]);
+    }
+  }
+
+  for (const headerData of pairsOfData) {
+    assertNotEquals(
+      aUriGenerator.generateIdFromBrowser(headerData[0]),
+      aUriGenerator.generateIdFromBrowser(headerData[1])
+    );
   }
 });

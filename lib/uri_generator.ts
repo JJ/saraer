430577bc54cb7f5961ef0c348c66887497ed87cb
JJ Merelo <jjmerelo@gmail.com>
2023-10-02 14:49:16 +0200

    Closes #1: Created data as an interface

--- lib/uri_generator.ts
+++ lib/uri_generator.ts
@@ -1,19 +1,21 @@
 import { crypto } from "https://deno.land/std@0.203.0/crypto/mod.ts";
 
-class URIgenerator {
+interface HeaderData {
+  user_agent: string;
+  accept: string;
+  accept_language: string;
+}
+
+export class URIgenerator {
   private encoder: TextEncoder;
 
   constructor() {
     this.encoder = new TextEncoder();
   }
 
-  generateIdFromBrowser({
-    user_agent: string,
-    accept: string,
-    accept_language: string,
-  }) {
+  generateIdFromBrowser({ user_agent, accept, accept_language }: HeaderData) {
     const collated = `${user_agent} | ${accept} | ${accept_language}`;
-    const data = this.encoder.encode(string);
+    const data = this.encoder.encode(collated);
     return crypto.subtle.digestSync("SHA-256", data);
   }
 }

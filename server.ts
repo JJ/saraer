64ac39ffa7300b3f1a7f2d1c47b1e914c15f2ab2
JJ Merelo <jjmerelo@gmail.com>
2023-10-03 20:52:07 +0200

    :poop:

--- server.ts
+++ server.ts
@@ -4,31 +4,29 @@ import { HeaderData, URIgenerator } from "./lib/uri_generator.ts";
 // The "tickets" route will return beer ticket
 // The "beers" route will store the request and return a green tick mark
 
-const deploymentURL= Deno.env.get("DEPLOYMENT_URL") || "http://localhost:8000";
+const deploymentURL = Deno.env.get("DEPLOYMENT_URL") || "http://localhost:8000";
 
 // Create a URI generator
 const generator = new URIgenerator(deploymentURL);
 
 Deno.server(async (_req) => {
-    // Parse the request path
-    const path = (new URL(_req.url)).pathname
-    const headers = _req.r.headers;
-    const [ route, sessionId, talkId, userId] = path.split("/");
-    // If the route is "tickets", generate a ticket
-    if ( route === "ticket") {
-        // Generate a ticket
-        const ticket = await generator.generateQRCode(sessionId, talkId, {
-            user_agent: headers.get("user-agent") || "",
-            accept: headers.get("accept") || "",
-            accept_language: headers.get("accept-language") || "",
-        });
-        // Return the ticket as a base64 encoded GIF
-        return new Response(ticket, {
-            headers: {
-                "content-type": "image/gif",
-            },
-        });
-    }
-);
-
-
+  // Parse the request path
+  const path = new URL(_req.url).pathname;
+  const headers = _req.r.headers;
+  const [route, sessionId, talkId, userId] = path.split("/");
+  // If the route is "tickets", generate a ticket
+  if (route === "ticket") {
+    // Generate a ticket
+    const ticket = await generator.generateQRCode(sessionId, talkId, {
+      user_agent: headers.get("user-agent") || "",
+      accept: headers.get("accept") || "",
+      accept_language: headers.get("accept-language") || "",
+    });
+    // Return the ticket as a base64 encoded GIF
+    return new Response(ticket, {
+      headers: {
+        "content-type": "image/gif",
+      },
+    });
+  }
+});

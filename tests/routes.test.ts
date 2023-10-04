6084fdf53b0fd67bac5e27af66c624d857573b2a
JJ Merelo <jjmerelo@gmail.com>
2023-10-04 10:51:26 +0200

    #12 I mean, WTF :white_check_mark: :clap:
    This took a short while with CoPilot

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -19,7 +19,6 @@ Deno.test(async function testTicketRoute() {
   assert(response.headers.get("content-type") === "image/gif");
   const body = await response.text();
   assert(body.startsWith("data:image/gif;base64,"));
-  // save body as gif file
   const data = body.slice(22);
   const bytes = Uint8Array.from(atob(data), (c) => c.charCodeAt(0));
   await Deno.writeFile("tests/test.gif", bytes);
@@ -27,5 +26,23 @@ Deno.test(async function testTicketRoute() {
   // run external command zbarimg on that file
   const qr_decoder = new Deno.Command("zbarimg", { args: ["tests/test.gif"] });
   const { code, stdout, stderr } = await qr_decoder.output();
-  console.log(code, stdout, stderr);
+  assert(code === 0);
+
+  // decode stdout to string
+  const decoder = new TextDecoder();
+  const decodedOutput = decoder.decode(stdout);
+  console.log(decodedOutput);
+
+  // check that the output is correct
+  assert(decodedOutput.startsWith("QR-Code:"));
+
+  // check that the output contains the sessionId
+  assert(decodedOutput.includes(ticketData[0]));
+
+  // check that the output contains the talkId
+  assert(decodedOutput.includes(ticketData[1]));
+
+  // check that the output contains a digest as last fragment
+  const digest = decodedOutput.split("/").pop();
+  assert(digest !== undefined);
 });

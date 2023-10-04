50d2508c8dd017799f5b310c17d32fa105dd8efa
JJ Merelo <jjmerelo@gmail.com>
2023-10-04 17:28:10 +0200

    :memo: for tests

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -4,40 +4,45 @@ import { assert } from "https://deno.land/std@0.203.0/assert/assert.ts";
 
 const aUriGenerator = new URIgenerator("https://test.data/test/");
 
-Deno.test(async function testTicketRoute() {
-  const ticketData = ["session-id", "talk-id"];
-  const headers = new Headers();
-  headers.set("user-agent", "test-user-agent");
-  headers.set("accept", "test-accept");
-  headers.set("accept-language", "test-accept-language");
+Deno.test(
+  "The ticket code should be generated correctly",
+  async function testTicketRoute() {
+    const ticketData = ["session-id", "talk-id"];
+    const headers = new Headers();
+    headers.set("user-agent", "test-user-agent");
+    headers.set("accept", "test-accept");
+    headers.set("accept-language", "test-accept-language");
 
-  const ticketImg = await ticket(aUriGenerator, ticketData, headers);
+    const ticketImg = await ticket(aUriGenerator, ticketData, headers);
 
-  assert(ticketImg.startsWith("data:image/gif;base64,"));
-  const data = ticketImg.slice(22);
-  const bytes = Uint8Array.from(atob(data), (c) => c.charCodeAt(0));
-  await Deno.writeFile("tests/test.gif", bytes);
+    assert(ticketImg.startsWith("data:image/gif;base64,"));
+    const data = ticketImg.slice(22);
+    const bytes = Uint8Array.from(atob(data), (c) => c.charCodeAt(0));
+    await Deno.writeFile("tests/test.gif", bytes);
 
-  const qr_decoder = new Deno.Command("zbarimg", { args: ["tests/test.gif"] });
-  const { code, stdout, stderr } = await qr_decoder.output();
+    const qr_decoder = new Deno.Command("zbarimg", {
+      args: ["tests/test.gif"],
+    });
+    const { code, stdout, stderr } = await qr_decoder.output();
 
-  await Deno.remove("tests/test.gif");
-  const decoder = new TextDecoder();
+    await Deno.remove("tests/test.gif");
+    const decoder = new TextDecoder();
 
-  assert(code === 0);
-  assert(
-    decoder
-      .decode(stderr)
-      .startsWith("scanned 1 barcode symbols from 1 images in")
-  );
-  const decodedOutput = decoder.decode(stdout);
+    assert(code === 0);
+    assert(
+      decoder
+        .decode(stderr)
+        .startsWith("scanned 1 barcode symbols from 1 images in")
+    );
+    const decodedOutput = decoder.decode(stdout);
 
-  assert(decodedOutput.startsWith("QR-Code:"));
+    assert(decodedOutput.startsWith("QR-Code:"));
 
-  assert(decodedOutput.includes(ticketData[0]));
+    assert(decodedOutput.includes(ticketData[0]));
 
-  assert(decodedOutput.includes(ticketData[1]));
+    assert(decodedOutput.includes(ticketData[1]));
 
-  const digest = decodedOutput.split("/").pop();
-  assert(digest !== undefined);
-});
+    const digest = decodedOutput.split("/").pop();
+    assert(digest !== undefined);
+  }
+);

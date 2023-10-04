3f792d9a5aefcc4bdede0330142626bd068d0317
JJ Merelo <jjmerelo@gmail.com>
2023-10-04 17:45:37 +0200

    :recycle: to use semantic assertions

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -1,6 +1,10 @@
 import { URIgenerator } from "../lib/uri_generator.ts";
 import { router, ticket } from "../lib/routes.ts";
-import { assert } from "https://deno.land/std@0.203.0/assert/assert.ts";
+import {
+  assertEquals,
+  assertExists,
+  assertStringIncludes,
+} from "https://deno.land/std@0.174.0/testing/asserts.ts";
 
 const aUriGenerator = new URIgenerator("https://test.data/test/");
 
@@ -15,7 +19,7 @@ Deno.test(
 
     const ticketImg = await ticket(aUriGenerator, ticketData, headers);
 
-    assert(ticketImg.startsWith("data:image/gif;base64,"));
+    assertEquals(ticketImg.startsWith("data:image/gif;base64,"), true);
     const data = ticketImg.slice(22);
     const bytes = Uint8Array.from(atob(data), (c) => c.charCodeAt(0));
     await Deno.writeFile("tests/test.gif", bytes);
@@ -28,28 +32,39 @@ Deno.test(
     await Deno.remove("tests/test.gif");
     const decoder = new TextDecoder();
 
-    assert(code === 0);
-    assert(
+    assertEquals(code, 0);
+    assertEquals(
       decoder
         .decode(stderr)
-        .startsWith("scanned 1 barcode symbols from 1 images in")
+        .startsWith("scanned 1 barcode symbols from 1 images in"),
+      true
     );
     const decodedOutput = decoder.decode(stdout);
 
-    assert(decodedOutput.startsWith("QR-Code:"));
+    assertEquals(decodedOutput.startsWith("QR-Code:"), true);
 
-    assert(decodedOutput.includes(ticketData[0]));
+    assertStringIncludes(decodedOutput, ticketData[0]);
 
-    assert(decodedOutput.includes(ticketData[1]));
+    assertStringIncludes(decodedOutput, ticketData[1]);
 
     const digest = decodedOutput.split("/").pop();
-    assert(digest !== undefined);
+    assertExists(digest);
   }
 );
 
+const ourRouter = router(aUriGenerator);
+
 Deno.test("The router should return a 404 for an unknown route", async () => {
-  const ourRouter = router(aUriGenerator);
   const request = new Request("https://test.data/unknown-route");
   const response = await ourRouter(request);
-  assert(response.status === 404);
+  assertEquals(response.status, 404);
+});
+
+Deno.test("The router should return a ticket for a known route", async () => {
+  const request = new Request("https://test.data/ticket/session-id/talk-id");
+  const response = await ourRouter(request);
+  assertEquals(response.status, 200);
+  const text = await response.text();
+  assertEquals(text.startsWith("<img src='data:image/gif;base64,"), true);
+  assertEquals(response.headers.get("content-type"), "text/html");
 });

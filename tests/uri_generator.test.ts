75f3f988da3f082e83c3d68e8ab48778f523c7a2
JJ Merelo <jjmerelo@gmail.com>
2023-10-04 19:16:01 +0200

    #17 :white_check_mark:

--- tests/uri_generator.test.ts
+++ tests/uri_generator.test.ts
@@ -1,5 +1,6 @@
 // generate test for URIgenerator
 import {
+  assertEquals,
   assertInstanceOf,
   assertNotEquals,
   assertStringIncludes,
@@ -24,24 +25,37 @@ for (const i in userAgentData) {
 
 const aUriGenerator = new URIgenerator("https://test.data/test/");
 
-Deno.test(function testUserAgentDataProcessor() {
-  for (const headerData of pairsOfData) {
-    assertNotEquals(
-      aUriGenerator.generateIdFromBrowser(headerData[0]),
-      aUriGenerator.generateIdFromBrowser(headerData[1])
-    );
-  }
-});
+Deno.test(
+  "Tests consistency of uri generators",
+  function testUserAgentDataProcessor() {
+    for (const headerData of userAgentData) {
+      const generatedId = aUriGenerator.generateIdFromBrowser(headerData);
+      const generatedIdFromCache =
+        aUriGenerator.generateIdFromBrowser(headerData);
+      assertEquals(generatedId, generatedIdFromCache);
+    }
 
-Deno.test(function testUriGenerator() {
-  for (const header of userAgentData) {
-    const sessionId = "session-id";
-    const talkId = "talk-id";
-    const uri = aUriGenerator.generateUri(sessionId, talkId, header);
-    const realUri = new URL(uri);
-    assertInstanceOf(realUri, URL);
+    for (const headerData of pairsOfData) {
+      assertNotEquals(
+        aUriGenerator.generateIdFromBrowser(headerData[0]),
+        aUriGenerator.generateIdFromBrowser(headerData[1])
+      );
+    }
   }
-});
+);
+
+Deno.test(
+  "URIs are generated with the correct class and format",
+  function testUriGenerator() {
+    for (const header of userAgentData) {
+      const sessionId = "session-id";
+      const talkId = "talk-id";
+      const uri = aUriGenerator.generateUri(sessionId, talkId, header);
+      const realUri = new URL(uri);
+      assertInstanceOf(realUri, URL);
+    }
+  }
+);
 
 Deno.test(async function testUriGenerator() {
   for (const header of userAgentData) {

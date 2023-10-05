5ef8f1b0fe13111f1243d46b1fc13544d54ab4d1
JJ Merelo <jjmerelo@gmail.com>
2023-10-05 17:39:20 +0200

    Adapt environment variables #14

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import { URIgenerator } from "./lib/uri_generator.ts";
 import { router } from "./lib/routes.ts";
+import { BeerBucket } from "./lib/beers.ts";
 
 // Serve two routes, one for "tickets" and one for "beers
 // The "tickets" route will return beer ticket
@@ -7,11 +8,13 @@ import { router } from "./lib/routes.ts";
 
 const deploymentURL = Deno.env.get("DEPLOYMENT_URL") || "http://localhost";
 const deploymentPort = Deno.env.get("DEPLOYMENT_PORT");
+const BEERS = parseInt(Deno.env.get("BEERS") || "10");
 
 // Create a URI generator
 const generator = new URIgenerator(deploymentURL);
+const beerBucket = new BeerBucket(BEERS);
 let config = {};
 if (deploymentPort) {
   config = { port: parseInt(deploymentPort) };
 }
-Deno.serve(config, router(generator));
+Deno.serve(config, router(generator, beerBucket));

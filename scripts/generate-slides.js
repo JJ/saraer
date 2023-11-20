4a1f25038d0ee5357021e233f455bd75df440598
JJ Merelo <jjmerelo@gmail.com>
2023-11-20 20:40:03 +0100

    Generating slides and templates

--- scripts/generate-slides.js
+++ scripts/generate-slides.js
@@ -1,31 +1,52 @@
 import PPTX from "nodejs-pptx";
+import { readFileSync } from "fs";
 
-let pptx = new PPTX.Composer();
+const pptx = new PPTX.Composer();
 
-await pptx.load(`assets/plantilla.pptx`);
-await pptx.compose(async (pres) => {
-  await pres.getSlide("slide1").addText((text) => {
-    text
-      .value("Título")
-      .x(50)
-      .y(10)
-      .cx(400)
-      .cy(400)
-      .fontSize(40)
-      .textColor("FF0000");
-  });
-  await pres.getSlide("slide1").addText((text) => {
-    text
-      .value("Nombre Persona")
-      .x(50)
-      .y(50)
-      .cx(400)
-      .cy(400)
-      .fontSize(30)
-      .textColor("FFFF00");
-  });
-  await pres.getSlide("slide2").addImage((image) => {
-    image.file(`/tmp/session-0-talk-1.png`).x(300).y(100).cx(400).cy(400);
-  });
-});
-await pptx.save(`/tmp/prueba.pptx`);
+const text = readFileSync("talks.txt");
+const talks = text.toString().split("\n");
+
+let titleAndAuthors = Array.from(
+  talks.map((talk) => {
+    const [title, authors] = talk.split(".");
+    return { title, authors };
+  })
+);
+
+console.log(titleAndAuthors);
+titleAndAuthors.pop();
+
+for (let i = 4; i >= 1; i--) {
+  for (let j = 4; j >= 1; j--) {
+    const talk = titleAndAuthors.pop();
+    const { title, authors } = talk;
+    console.log(talk, title, authors);
+    await pptx.load(`assets/plantilla.pptx`);
+    await pptx.compose(async (pres) => {
+      await pres.getSlide("slide1").addText((text) => {
+        text
+          .value(title)
+          .x(50)
+          .y(2)
+          .cx(400)
+          .cy(200)
+          .fontSize(40)
+          .textColor("FF0000");
+      });
+      await pres.getSlide("slide1").addText((text) => {
+        text
+          .value(authors)
+          .x(200)
+          .y(100)
+          .cx(400)
+          .cy(300)
+          .fontSize(30)
+          .textColor("FFFF00");
+      });
+      await pres.getSlide("slide2").addImage((image) => {
+        image.file(`/tmp/session-0-talk-1.png`).x(300).y(100).cx(400).cy(400);
+      });
+    });
+    await pptx.save(`assets/plantilla-sesion-${j}-charla-${i}.pptx`);
+  }
+}

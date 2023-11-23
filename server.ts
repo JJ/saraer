fca80dc54c0ee2ef3aa01fd2cbd23c049a513288
JJ Merelo <jjmerelo@gmail.com>
2023-11-23 12:33:33 +0100

    Incrementa el número de cervezas

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { BeerBucket } from "./lib/beers.ts";
 
 const deploymentURL = Deno.env.get("DEPLOYMENT_URL") || "http://localhost";
 const deploymentPort = Deno.env.get("DEPLOYMENT_PORT");
-const BEERS = parseInt(Deno.env.get("BEERS") || "1000");
+const BEERS = parseInt(Deno.env.get("BEERS") || "2000");
 const beerRoute = Deno.env.get("BEER_ROUTE") || "beer";
 
 const generator = new URIgenerator(`${deploymentURL}/${beerRoute}`);
